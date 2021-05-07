c197ad0549ac0ba15f0a5ce8f1e87243a622a059
Sidharth S <74829800+Sidharth-s-01@users.noreply.github.com>
2021-05-08 01:13:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+$("img").on("click",function(){
+  animal_id=this.getAttribute("id")
+  musicplay(animal_id);
+
+});
+
+
+
+$(".animal-button").click(function(){
+var animal=this.innerHTML;
+
+if(animal=="Tiger")
+{
+  $(".winner-title").html("You got it right");
+}
+else{
+  $(".winner-title").html("You got it wrong");
+}
+
+set
+
+}
+)
+
+
+
+
+function musicplay(clas){
+  switch (clas) {
+    case "tiger":
+      var audio1=new Audio("sounds/tiger.mp3");
+      audio1.play();
+      break;
+    case "lion":
+      var audio2=new Audio("sounds/lion.mp3");
+      audio2.play();
+      break;
+    case "elephant":
+        var audio3=new Audio("sounds/ELEPHANT.WAV");
+        audio3.play();
+        break;
+    default:
+
+  }
+
+}
+
+var repeater;
+function screen_width(){
+  if(window.innerWidth<920)
+  {
+    $(".non-mobile-animal").addClass("mobile-animal");
+    $(".change-needed").removeClass("mobile-animal");
+  }
+  else{
+    $(".change-needed").addClass("mobile-animal");
+      $(".non-mobile-animal").removeClass("mobile-animal");
+  }
+  repeater=setTimeout(screen_width,.100);
+}
+
+screen_width()
